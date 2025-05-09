8dea19aa0dd9b1351fd9bd31f880191af027687d
squaresheep28@gmail.com <reallypurple@mailmask.com>
2025-05-09 14:56:02 +0300

    update index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var sound = document.getElementById("button_sound");
-var btn = document.getElementById("button");
+var btn = document.getElementById("btn");
 
 btn.addEventListener('click', function() {
     var buttonSound2 = new Audio('button.wav');
-
+    buttonSound2.volume = 0.3;
     buttonSound2.play();
 });
