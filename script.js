0e37324c31eb75eb2b83dd1fbc319ef051eac787
squaresheep28@gmail.com <reallypurple@mailmask.com>
2025-05-13 19:29:33 +0300

    delete 2 files and update 2 files

--- script.js
+++
@@ -1,8 +0,0 @@
-var sound = document.getElementById("button_sound");
-var btn = document.getElementById("btn");
-
-btn.addEventListener('click', function() {
-    var buttonSound2 = new Audio('button.wav');
-    buttonSound2.volume = 0.3;
-    buttonSound2.play();
-});
