ec86dfb75a4a6d1955bc53373049b5ef767637e2
freezicles <squaresheep28@gmail.com>
2025-06-26 20:39:36 +0300

    delete 3 files and update 2 files

---
+++ script.js
@@ -0,0 +1,22 @@
+const cursor = document.getElementById('cursor');
+let mouseX = 0, mouseY = 0;
+let posX = 0, posY = 0;
+const speed = 0.2;
+
+document.addEventListener('mousemove', e => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+function animate() {
+  posX = lerp(posX, mouseX, speed);
+  posY = lerp(posY, mouseY, speed);
+  cursor.style.transform = `translate(${posX}px, ${posY}px) translate(-50%, -50%)`;
+  requestAnimationFrame(animate);
+}
+
+animate();
