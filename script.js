45922f545e7e63d9cbd6d711dcfbc4e9cf872337
freezicles <squaresheep28@gmail.com>
2026-02-21 18:33:29 +0200

    update index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('particles.js loaded');
 });
+
+console.log(
+  "%cFreezicles Labs",
+  `
+  font-size: 50px;
+  display: inline-block;
+  background: black;
+  color: lime;
+  `
+);
+console.log(
+  "%cshh.. there's nothing here..",
+  `
+  font-size: 20px;
+  display: inline-block;
+  background: black;
+  color: lime;
+
+  `
+);
+const devUrl = window.location.origin + "/dev";
+
+console.log(
+  "%cfine here you go... " + devUrl,
+  `
+  font-size: 15px;
+  color: red;
+  `
+);
